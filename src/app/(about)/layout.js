7d3680a55871f079a7e50cf1260122ec02826d71
AndreiDuD <andrei.duduman123@gmail.com>
2023-10-02 12:21:01 +0300

    updated skills and insights

--- src/app/(about)/layout.js
+++ src/app/(about)/layout.js
@@ -1,15 +1,11 @@
 import InsightRoll from "@/src/components/About/InsightRoll";
 
-
 const insights = [
-    "20+ Projects Completed",
-    "3+ Years of Freelancing",
-    "99% Client Satisfaction",
-    "20K+ Subscribers",
-    "Authored In-Depth Course on Educative",
-    "Contributed as a Technical Course Reviewer 📝",
-    "Recipient of the Hackernoon Noonies Award 🏆",
-  ];
+  "3+ Web Projects Completed",
+  "3+ Azure Cloud Projects Completed",
+  "2+ Python Projects Completed",
+  "1+ Years Experience in Data Analysis",
+];
 
 export default function AboutLayout({ children }) {
   return (
