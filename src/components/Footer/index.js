a06870d3f15efa3dcb01bb45d6e4ccf9708a4e22
AndreiDuD <andrei.duduman123@gmail.com>
2023-10-02 11:57:52 +0300

    updated footer link

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -80,7 +80,7 @@ const Footer = () => {
         <div className="text-center">
           Made with &hearts; by{" "}
           <a
-            href="https://devdreaming.com"
+            href="https://portfolio-andrei-duduman.vercel.app/"
             className="underline"
             target="_blank"
           >
